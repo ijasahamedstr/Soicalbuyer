c9acf4c3ac77e2f1422defe511bb1a0677e83d2b
ijasahamedstr <ijasahamed.str@gmail.com>
2024-10-14 11:17:54 +0300

    commit

--- frontend/src/component/Page/LoginPage/LoginAPI/ApiCall.js
+++ frontend/src/component/Page/LoginPage/LoginAPI/ApiCall.js
@@ -8,8 +8,18 @@ export const commonrequest = async(methods,url,body,header)=>{
         :{
             "Content-Type":"application/json"
         },
-        data:body
+        data:body,
+        body: JSON.stringify({ email }),
     }
+
+    if (!response.ok) {
+        const errorDetails = await response.json();
+        console.error("API Error:", errorDetails);
+        throw new Error(errorDetails.error);
+    }
+
+    const data = await response.json();
+    
 
     // axios instance
     return axios(config).then((data)=>{
